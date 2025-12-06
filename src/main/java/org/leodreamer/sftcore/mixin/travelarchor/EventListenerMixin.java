89ae7aa434bf075866189213ef1d199c8ae194e3
LeoDreamer <2200010825@stu.pku.edu.cn>
2025-12-06 19:52:47 +0800

    旅行权杖冷却

---
+++ src/main/java/org/leodreamer/sftcore/mixin/travelarchor/EventListenerMixin.java
@@ -0,0 +1,20 @@
+package org.leodreamer.sftcore.mixin.travelarchor;
+
+import de.castcrafter.travelanchors.EventListener;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemCooldowns;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(EventListener.class)
+public class EventListenerMixin {
+
+    @Redirect(method = "onRightClick", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/item/ItemCooldowns;addCooldown(Lnet/minecraft/world/item/Item;I)V"
+    ))
+    private void travelAnchors$skipCooldown(ItemCooldowns cooldowns, Item item, int coolDownTicks) {
+        // Do nothing
+    }
+}
