b7174550eebe0c681cb9598609b57fbcdcc17f1c
LeoDreamer <2200010825@stu.pku.edu.cn>
2026-06-08 00:48:50 +0800

    implement me gas output hatch

--- src/main/java/org/leodreamer/sftcore/api/gui/AEChemicalDisplayWidget.java
+++ src/main/java/org/leodreamer/sftcore/api/gui/AEChemicalDisplayWidget.java
@@ -3,54 +3,48 @@ package org.leodreamer.sftcore.api.gui;
 import com.gregtechceu.gtceu.api.gui.GuiTextures;
 import com.gregtechceu.gtceu.integration.ae2.gui.widget.slot.AEConfigSlotWidget;
 import com.gregtechceu.gtceu.utils.FormattingUtil;
-import com.lowdragmc.lowdraglib.gui.util.DrawerHelper;
-import com.lowdragmc.lowdraglib.gui.widget.WidgetGroup;
-import me.ramidzkh.mekae2.ae2.MekanismKey;
-import mekanism.api.chemical.gas.GasStack;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
+
+import com.lowdragmc.lowdraglib.gui.util.DrawerHelper;
+import com.lowdragmc.lowdraglib.gui.widget.Widget;
+import com.lowdragmc.lowdraglib.utils.Position;
+import com.lowdragmc.lowdraglib.utils.Size;
+import me.ramidzkh.mekae2.ae2.MekanismKey;
+import mekanism.api.chemical.gas.GasStack;
+import mekanism.client.gui.GuiUtils;
+import mekanism.client.render.MekanismRenderer;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
-public class AEChemicalDisplayWidget extends WidgetGroup {
+public class AEChemicalDisplayWidget extends Widget {
 
     private final AEChemicalListGridWidget gridWidget;
     private final int index;
 
     public AEChemicalDisplayWidget(int x, int y, AEChemicalListGridWidget gridWidget, int index) {
-        super(x, y, 18, 18);
+        super(new Position(x, y), new Size(18 + 140, 18));
         this.gridWidget = gridWidget;
         this.index = index;
-
-        var gasSlot = new GasTankWidget(
-            new DisplayGasHandler(List.of(getDisplayGas())),
-            0, 0, 0, 18, 18
-        )
-            .setShowAmount(false)
-            .setShowAmountOverlay(false)
-            .setDrawHoverTips(false)
-            .setDrawHoverOverlay(false)
-            .setAllowClickFilled(false)
-            .setAllowClickDrained(false)
-            .setClientSideWidget();
-        this.addWidget(gasSlot);
     }
 
     @Override
     public void drawInBackground(@NotNull GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
+        super.drawInBackground(graphics, mouseX, mouseY, partialTicks);
+
         var pos = getPosition();
 
         GuiTextures.FLUID_SLOT.draw(graphics, mouseX, mouseY, pos.x, pos.y, 18, 18);
         GuiTextures.NUMBER_BACKGROUND.draw(graphics, mouseX, mouseY, pos.x + 18, pos.y, 140, 18);
 
-        super.drawInBackground(graphics, mouseX, mouseY, partialTicks);
-
         var gas = getDisplayGas();
         if (!gas.isEmpty()) {
+            drawGas(graphics, gas, pos.x + 1, pos.y + 1);
+
             String amountStr = "x" + FormattingUtil.formatNumbers(gas.getAmount()) + "B";
             DrawerHelper.drawText(graphics, amountStr, pos.x + 21, pos.y + 5, 1, 0xFFFFFFFF);
         }
@@ -58,6 +52,34 @@ public class AEChemicalDisplayWidget extends WidgetGroup {
         if (isMouseOverElement(mouseX, mouseY)) {
             AEConfigSlotWidget.drawSelectionOverlay(graphics, pos.x + 1, pos.y + 1, 16, 16);
         }
+    }
+
+    /**
+     * easy version to display gas instead of {@link DisplayGasHandler}
+     */
+    private void drawGas(GuiGraphics graphics, GasStack gas, int x, int y) {
+        if (gas.isEmpty()) {
+            return;
+        }
+
+        var sprite = MekanismRenderer.getChemicalTexture(gas.getType());
+
+        MekanismRenderer.color(graphics, gas);
+        GuiUtils.drawTiledSprite(
+            graphics,
+            x,
+            y + 16,
+            0,
+            16,
+            16,
+            sprite,
+            16,
+            16,
+            100,
+            GuiUtils.TilingDirection.UP_RIGHT,
+            true
+        );
+        MekanismRenderer.resetColor(graphics);
     }
 
     @Override
@@ -73,11 +95,13 @@ public class AEChemicalDisplayWidget extends WidgetGroup {
 
         var tooltip = new ArrayList<Component>();
         tooltip.add(gas.getTextComponent());
-        tooltip.add(Component.translatable(
-            GasTankWidget.GAS_STORED,
-            FormattingUtil.formatNumbers(gas.getAmount()),
-            FormattingUtil.formatNumbers(gas.getAmount())
-        ));
+        tooltip.add(
+            Component.translatable(
+                GasTankWidget.GAS_STORED,
+                FormattingUtil.formatNumbers(gas.getAmount()),
+                FormattingUtil.formatNumbers(gas.getAmount())
+            )
+        );
 
         graphics.renderTooltip(
             Minecraft.getInstance().font,
